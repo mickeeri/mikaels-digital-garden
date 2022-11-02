6aeebd703443933a1077e598da2ec4ee3cbbfa82
mikael-eriksson1 <mikael.a.eriksson@skf.com>
2022-11-02 10:21:54 +0100

    Add link to start page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,17 @@
+import { Link } from "@remix-run/react";
+
 export default function IndexRoute() {
-  return <div>Hello Index Route</div>;
+  return (
+    <div>
+      <h1>Hello Index Route</h1>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="posts">All posts</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
